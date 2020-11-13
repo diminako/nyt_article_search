48b0e02927085f08e39cfe11a9db2bea3f4ca7fc
DimitriNakos <dimi1nakos@gmail.com>
2020-11-13 14:03:14 -0500

    saving

--- script.js
+++ script.js
@@ -3,17 +3,26 @@ var searchTerm = $("#searchTerm")
 var recordNumber = $("recordNumber")
 var startYear = $("startYear")
 var endYear = $("endYear")
-var submitBtn = $("#submitBtn")
-var clearBtn = $("#clearBtn")
+var submitBtn = $("#search")
+var clearBtn = $("#clear")
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + APIkey
 
 
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response)
-})
+function searchResults() {
 
+}
+
+
+submitBtn.on("click", function (event) {
+    event.preventDefault();
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+    })
+
+})
