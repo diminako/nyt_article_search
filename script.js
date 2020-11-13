834eb121b6457bb3123bfdbaccf82ca97012d894
DimitriNakos <dimi1nakos@gmail.com>
2020-11-13 14:51:57 -0500

    trying to git pull

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var searchTerm = $("#searchTerm")
 var recordNumber = $("recordNumber")
 var startYear = $("startYear")
 var endYear = $("endYear")
-var submitBtn = $("#search")
-var clearBtn = $("#clear")
+var submitBtn = $("#submitBtn")
+var clearBtn = $("#clearBtn")
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + APIkey
 
@@ -17,12 +17,24 @@ function searchResults() {
 
 submitBtn.on("click", function (event) {
     event.preventDefault();
+    var APIkey = "JYxnRLGDQhYPQh5CJ7txIMYPBlmv12Zj"
+
+    var search = searchTerm.val()
+    var record = recordNumber.val()
+    var start = startYear.val()
+    var end = endYear.val()
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&facet_fields=source&facet=true&begin_date=" + start + "0101&end_date=" + end + "0101&api-key=" + APIkey
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response)
+        console.log(response.response.docs[0].headline.main)
+        // response.docs[0].headline.print_headline
+
+        // response.docs[0]
     })
 
 })
