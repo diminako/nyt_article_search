0c901d922b09a38d51d4d493bfae81cda73eaf59
DimitriNakos <dimi1nakos@gmail.com>
2020-11-13 13:49:39 -0500

    script push

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 var APIkey = "JYxnRLGDQhYPQh5CJ7txIMYPBlmv12Zj"
+var searchTerm = $("#searchTerm")
+var recordNumber = $("recordNumber")
+var startYear = $("startYear")
+var endYear = $("endYear")
+var submitBtn = $("#submitBtn")
+var clearBtn = $("#clearBtn")
+
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + APIkey
+
+
 
 $.ajax({
     url: queryURL,
@@ -7,3 +16,4 @@ $.ajax({
 }).then(function (response) {
     console.log(response)
 })
+
